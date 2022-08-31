b38a517eb07234b761870b3ca0b573186eef0784
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-31 13:56:30 +0300

    Changed html structure

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ recipeCloseBtn.addEventListener('click',()=>{
     cocktailDetails.parentElement.classList.remove('showRecipe');
     } );
 
-//get cockatail that matches ingredients
+//get cocktail that matches ingredients
 
 function getCocktailList(){
     let searchInputText=document.getElementById('search-input').value.trim();
@@ -25,7 +25,7 @@ function getCocktailList(){
                         <div class = "cocktail-img">
                             <img src = "${cocktail.strCocktailThumb}" alt = "food">
                         </div>
-                        <div class = "cockatail-name">
+                        <div class = "cocktail-name">
                             <h3>${cocktail.strCocktail}</h3>
                             <a href = "#" class = "recipe-btn">Get Recipe</a>
                         </div>
@@ -40,4 +40,37 @@ function getCocktailList(){
     }
         cocktailList.innerHTML=html;
 });
+}
+
+//get cocktail recipe
+function getCocktailRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let cocktailItem=e.target.parentElement.parentElement;
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka'+cocktailItem.dataset.id)
+        .then(response=>response.json())
+        .then(data=>getCocktailRecipeModal(data.cocktail));
+    }
+}
+
+//create modal
+function cocktailRecipeModal(cocktail){
+    console.log(cocktail)
+    cocktail=cocktail[0];
+    let html=`
+        <h2 class = "recipe-title">${cocktail.strCocktail}</h2>
+        <p class = "recipe-category">${cocktail.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${cocktail.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${cocktail.strCocktailThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${cocktail.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+`;
+    cocktailDetails.innerHTML=html;
+    cocktailDetails.parentElement.classList.add('showRecipe');
 }

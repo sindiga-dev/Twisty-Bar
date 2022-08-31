09a38d6f98a4ea358aa83d40d4afa371fda1bf34
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-31 11:41:21 +0300

    Declares variables

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-const searchButton=document.getElementById('search-btn');
-const cocktailList=document.getElementById('cocktail-list');
-
-//event listener for search button
-
-
-
+const searchButton=document.getElementById('search-button');
+const cocktailList=document.getElementById('cocktail');
+const cocktailDetails=document.getElementById('cocktail-details-content');
+const recipeCloseBtn=document.getElementById('recipe-close-btn');

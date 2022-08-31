4ba04484360e9083168b5cd2a01205dddbee264a
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-31 12:17:55 +0300

    Adds get cocktail function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const searchButton=document.getElementById('search-button');
+const searchButton=document.getElementById('search-btn');
 const cocktailList=document.getElementById('cocktail');
 const cocktailDetails=document.getElementById('cocktail-details-content');
 const recipeCloseBtn=document.getElementById('recipe-close-btn');
@@ -9,3 +9,35 @@ cocktailList.addEventListener('click',getCocktailRecipe);
 recipeCloseBtn.addEventListener('click',()=>{
     cocktailDetails.parentElement.classList.remove('showRecipe');
     } );
+
+//get cockatail that matches ingredients
+
+function getCocktailList(){
+    let searchInputText=document.getElementById('search-input').value.trim();
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka${searchInputText}`)
+    .then(response=>response.json())
+    .then(data=>{
+        let html='';
+        if(data.cocktail){
+            data.cocktail.forEach(cocktail=>{
+                html+=`
+                    <div class = "cocktail-item" data-id = "${cocktail.idCocktail}">
+                        <div class = "cocktail-img">
+                            <img src = "${cocktail.strCocktailThumb}" alt = "food">
+                        </div>
+                        <div class = "cockatail-name">
+                            <h3>${cocktail.strCocktail}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+             `;
+            
+            });
+        cocktailList.classList.remove('notFound');
+    }else{
+        html="sorry, we didn't find any cocktails!";
+        cocktailList.classList.add('notFound');
+    }
+        cocktailList.innerHTML=html;
+});
+}

dc3cbd0d5f710182e14bc4731056519bac50f1b0
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-31 11:46:43 +0300

    Adds event listeners

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ const searchButton=document.getElementById('search-button');
 const cocktailList=document.getElementById('cocktail');
 const cocktailDetails=document.getElementById('cocktail-details-content');
 const recipeCloseBtn=document.getElementById('recipe-close-btn');
+
+//adding event listeners
+searchButton.addEventListener('click',getCocktailList);
+cocktailList.addEventListener('click',getCocktailRecipe);
+recipeCloseBtn.addEventListener('click',()=>{
+    cocktailDetails.parentElement.classList.remove('showRecipe');
+    } );
